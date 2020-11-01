a463f7ba4756a541f5515aa99b70e050d5a2f97c
jesus payares <j.payares2@gmail.com>
2020-10-31 21:42:10 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="success"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+    <v-container>
+    <v-main>
+      <router-view/>
+      
+      <v-dialog
+      v-model="loading.estado"
+      hide-overlay
+      persistent
+      width="300"
+    >
+      <v-card
+        :color="loading.color"
+        dark
+      >
+        <v-card-text>
+          {{loading.titulo}}
+          <v-progress-linear
+            indeterminate
+            color="white"
+            class="mb-0"
+          ></v-progress-linear>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+
+    </v-main>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+import {mapState} from "vuex";
+
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  computed:{
+    ...mapState(['loading'])
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
